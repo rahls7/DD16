53e1dc98de795c3d0e23c52dddeea2f474c4d212
fish <fishisycho@gmail.com>
2017-03-13 20:13:30 -0400

    [Adding] javadoc to Start class

--- src/view/Start.java
+++ src/view/Start.java
@@ -1,38 +1,65 @@
 package view;
 
 import controller.CampaignEditController;
+import controller.CharacterEditorController;
+import controller.MapEditorController;
 import org.json.JSONArray;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
+ * Start page for entering the game.
+ *
+ * @author Wenyu Gu
+ * @version 1.0.0
+ *
  * Created by Fish on 2017/3/11.
  */
 public class Start extends JPanel {
     private JPanel start_panel;
     private CampaignEditController campaign_controller;
-
+//    private CharacterEditorController character_controller;
     /**
-     * Create a start panel
+     * Create a start panel. The panel includes campaign options, character options and start game button.
      */
-    public Start (){
-        super(new GridLayout(1, 0));
-        campaign_controller = new CampaignEditController();
 
+    public Start (){
+        super(new GridLayout(1, 3));
+        campaign_controller = new CampaignEditController();
         start_panel = new JPanel();
+
         JComboBox<Integer> campaign = getCampaignList();
+        campaign.setBorder(BorderFactory.createTitledBorder("Campaign:"));
         start_panel.add(campaign);
 
+//        JComboBox<String> character = getCharacterList();
+//        character.setBorder(BorderFactory.createTitledBorder("Character:"));
+//        start_panel.add(character);
+
         JButton start_game = new JButton("Start Game");
+//        start_game.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                Main.mainFrame.getContentPane().removeAll();
+//                Main.mainFrame.getContentPane().add(new Play("123456", 5), BorderLayout.CENTER);
+//                Main.mainFrame.getContentPane().doLayout();
+//                repaint();
+//                validate();
+//                Main.mainFrame.setVisible(true);
+//            }
+//        });
         start_panel.add(start_game);
 
         add(start_panel);
     }
 
     /**
-     *Get the existing campaign list
-     * @return
+     *Get the existing campaign list from the file.
+     *
+     * @return List of the campaigns.
      */
     public JComboBox<Integer> getCampaignList(){
         JComboBox<Integer> campaigns = new JComboBox<Integer>();
@@ -43,4 +70,20 @@ public class Start extends JPanel {
         }
         return campaigns;
     }
+
+    /**
+     * Get the existing character lsit from the file.
+     *
+     * @return List of the characters.
+     */
+//    public JComboBox<String> getCharacterList() {
+//        JComboBox<String> characters = new JComboBox<String>();
+//        JSONArray json_items = character_controller.getCharacter();
+//
+//        for (int i = 0; i < json_items.length(); i++) {
+//            String character_id = json_items.getJSONObject(i).getString("id");
+//            characters.addItem(character_id);
+//        }
+//        return characters;
+//    }
 }
